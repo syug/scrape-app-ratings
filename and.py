5724d88c0660bd82134739df8ecf3c3c38162480
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-04-03 15:38:49 +0900

    removed comments

--- and.py
+++ and.py
@@ -5,20 +5,10 @@ import sys
 import re
 from bs4 import BeautifulSoup
 
-# https://play.google.com/store/apps/details?id=jp.co.rakuten.research&hl=ja
-
 def getRate(url):
   html = urllib2.urlopen(url)
-  # html = urllib2.urlopen("https://play.google.com/store/apps/details?id=jp.co.rakuten.research&hl=ja")
   soup = BeautifulSoup(html, 'lxml')
   
-  # container = soup.find(class_="pf5lIe")
-  # label = container.div['aria-label']
-  # start = re.match(u"平均評価: 星 ", label).end()
-  # end = re.search(u'/5', label).start()
-  # rate = label[start:end]
-
-  # <meta itemprop="ratingValue" content="3.9394569396972656">
   rate = soup.find(attrs={"itemprop": "ratingValue"})['content']
   return round(float(rate), 1)
 
