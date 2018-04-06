11ed1b167640cbafb05227f12be9fae8090d4dbc
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-04-06 09:34:04 +0900

    added emoji

--- and.py
+++ and.py
@@ -4,13 +4,21 @@ import urllib2
 import sys
 import re
 from bs4 import BeautifulSoup
+import emoji
 
 def getRate(url):
   html = urllib2.urlopen(url)
   soup = BeautifulSoup(html, 'lxml')
   
-  rate = soup.find(attrs={"itemprop": "ratingValue"})['content']
-  return round(float(rate), 1)
+  rateRaw = soup.find(attrs={"itemprop": "ratingValue"})['content']
+  rate = str(round(float(rateRaw), 1))
+  rateNum = int(round(float(rateRaw)))
+  stars = []
+  for i in range(rateNum):
+    stars.append(':star:')
+  thumsup = ':sparkles: :thumbs_up: :sparkles:' if (rateNum >= 4) else ''
+  return emoji.emojize('Your rating is: ' + ' '.join(stars) + ' (' + rate + ') ' + thumsup, use_aliases=True)
+  # return round(float(rate), 1)
 
 if __name__ == "__main__":
   for arg in sys.argv:
