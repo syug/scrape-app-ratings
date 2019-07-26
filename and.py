8d3f049bf99f10ceb31127c657e41d44c8a4f505
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2019-07-26 14:25:22 +0900

    updated - added a case for ld+json is none

--- and.py
+++ and.py
@@ -10,16 +10,21 @@ import emoji
 def getRatingValue(url):
   html = urllib2.urlopen(url)
   soup = BeautifulSoup(html, 'lxml')
-  
   jsonldString = soup.find(name="script", attrs={"type": "application/ld+json"})
+
+  if jsonldString == None:
+    return -1
+
   jsonld = json.loads(jsonldString.get_text())
   rateRaw = jsonld['aggregateRating']['ratingValue']
   return float(rateRaw)
 
 def getResultMessage(ratingValue):
-  rate = str(round(float(ratingValue), 1))
-  rateNum = int(round(float(ratingValue)))
+  rate = str(round(ratingValue, 1))
+  rateNum = int(round(ratingValue))
   stars = []
+  if rateNum < 1:
+    stars.append(':cry:')
   for i in range(rateNum):
     stars.append(':star:')
   thumsup = ':sparkles: :thumbs_up: :sparkles:' if (rateNum >= 4) else ''
@@ -30,6 +35,12 @@ if __name__ == "__main__":
   for arg in sys.argv:
     if arg != __file__:
       print('URL: ' + arg)
-      ratingValue = getRatingValue(arg)
-      message = getResultMessage(ratingValue=ratingValue)
+
+      ratingValue = getRatingValue(url=arg)
+
+      if ratingValue >= 0:
+        message = getResultMessage(ratingValue=ratingValue)
+      elif ratingValue == -1:
+        message = emoji.emojize(':exclamation: Can not find <script type="application/ld+json"> on this URL :exclamation:', use_aliases=True)
+
       print(message)
